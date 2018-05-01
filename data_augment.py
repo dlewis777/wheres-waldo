a347b16f5c9841464c0fd20589035d30d2190265
dlewis777 <lefranchboi@gmail.com>
2018-05-01 14:47:41 -0400

    setting up part 4

--- data_augment.py
+++ data_augment.py
@@ -4,11 +4,11 @@ import sys
 # only argument is directory of data
 
 wkdir = sys.argv[1]
-imsize = sys.argv[2] # 256, 128, 64
+imsize = int(sys.argv[2]) # 256, 128, 64
 
-for filename in  os.listdir(wkdir + '/' + imsize + '/waldo'):
+for filename in  os.listdir(wkdir + '/' + str(imsize) + '/waldo'):
     
-    im = cv2.imread(wkdir + '/' + imsize + '/waldo/' + filename)
+    im = cv2.imread(wkdir + '/' + str(imsize) + '/waldo/' + filename)
     #up/down
     for i in range(imsize):
         #left/right
@@ -16,6 +16,7 @@ for filename in  os.listdir(wkdir + '/' + imsize + '/waldo'):
 
             temp = cv2.copyMakeBorder(im,0,i,0,j,cv2.BORDER_WRAP)
             temp2 = temp[j:imsize+j+1,i:imsize+i+1]
-
-            cv2.imwrite(filename + '-' + i + '-' + j, temp2)
-
+            cv2.imshow('image', temp2)
+            cv2.imwrite(wkdir + '/' + str(imsize) + '/augment/' + filename + '-' + str(i) + '-' + str(j) + '.jpg', temp2)	
+            temp3 = cv2.flip(temp2,0)
+            cv2.imwrite(wkdir + '/' + str(imsize) + '/augment/' + filename + '-' + str(i) + '-' + str(j) + 'r.jpg', temp2)	
