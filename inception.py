7411017f53d9a16e61ae09b4585c6b49f3dfee5f
echiang3 <echiang3@users.noreply.github.com>
2018-05-02 01:41:23 -0400

    v3 training

--- inception.py
+++ inception.py
@@ -8,7 +8,6 @@ from torch.optim import lr_scheduler
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
 import time
 import os
 import copy
