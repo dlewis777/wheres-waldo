e6a0f097af6a70c12e64ee4bc5f6c0d65e83493e
echiang3 <echiang3@users.noreply.github.com>
2018-05-02 01:40:01 -0400

    edited first model

--- inception.py
+++ inception.py
@@ -1,6 +1,18 @@
 #from torch.utils.data import Dataset, ImageFolder
-from torchvision import transforms, utils, datasets
-import torchvision.models as models
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
 
 data_transform = transforms.Compose([
         #transforms.RandomSizedCrop(224),
