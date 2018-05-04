b4ac922b63fd2dfc668b2833f6b0eb4ed332c0a4
echiang3 <echiang3@users.noreply.github.com>
2018-05-03 23:56:56 -0400

    finished validate

--- validate.py
+++ validate.py
@@ -12,41 +12,17 @@ import time
 import os
 import copy
 
-
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(299),#resnet is 224
-        #transforms.RandomHorizontalFlip(),
-        #transforms.RandomVerticalFlip(),
-        transforms.RandomAffine(45, translate=(0.1,0.1), shear=45),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(299),
-	#transforms.RandomHorizontalFlip(),
-	#transforms.RandomVerticalFlip(),
-        transforms.RandomAffine(0,shear=15),
-	#transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
-
-#waldo_dataset = datasets.ImageFolder(root='data/256', transform=data_transform)
-
-#dataset_loader = torch.utils.data.DataLoader(waldo_dataset, batch_size=4, shuffle=True, num_workers=4)
+data_transform = transforms.Compose([
+    transforms.Resize(299).,
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
 data_dir = 'new_data'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
-                  for x in ['heldout']}
-image_datasets = datasets.ImageFolder(os.path.
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=64,
-                                             shuffle=True, num_workers=16)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
+image_dataset = datasets.ImageFolder(os.path.join(data_dir, 'heldout'), data_transform)
+dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=64, shuffle = False, num_workers = 16)
+dataset_sizes = len(image_dataset)
+class_names = image_datasets.classes
 
 if torch.cuda.is_available():
     print("Using GPU")
@@ -54,95 +30,41 @@ else:
     print("Using CPU")
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#device = torch.device("cpu") # for cuda error
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-    since = time.time()
+def test_model(model, ):
+    model.eval()
 
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
+    running_loss = 0.0
+    running_corrects = 0
 
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+    for inputs, labels in dataloaders[phase]:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                model.train()  # Set model to training mode
-                print('TRAINING')
-            else:
-                model.eval()   # Set model to evaluate mode
-                print('EVALUATING')
+        # zero the parameter gradients
+        optimizer.zero_grad()
 
-            running_loss = 0.0
-            running_corrects = 0
+        outputs = model(inputs)
 
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+        if isinstance(outputs, tuple):
+            _, preds = torch.max(outputs[0], 1)
+            loss = sum((criterion(o,labels) for o in outputs))
+        else:
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+        running_corrects += torch.sum(preds == labels.data)
 
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    #print('inputs shape: ', inputs.shape)
-                    outputs = model(inputs)
-                    #print('output[0] shape: ', outputs[0].shape)
-                    #print('output[1] shape: ', outputs[1].shape)
-                    #_, preds = torch.max(outputs[0], 1)
-                    #print('prediction shape: ', preds.shape)
-                    #print('labels shape: ', labels.data.shape)
-                    #print(labels)
-                    if isinstance(outputs, tuple):
-                        _, preds = torch.max(outputs[0], 1)
-                        loss = sum((criterion(o,labels) for o in outputs))
-                    else:
-                        _, preds = torch.max(outputs, 1)
-                        loss = criterion(outputs, labels)
-                    # loss = criterion(outputs[1], labels) + criterion(outputs[0], labels)
+    acc = running_corrects.double() / dataset_sizes
 
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
+    print('Accuracy: {:.4f}'.format(acc))
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
 
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+model = torch.load('inception_10epochs.pt')
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+model = model.to(device)
 
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
 
-        print()
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
-
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
-
-model_ft = models.inception_v3(pretrained=True)
-#model_ft = models.resnet18(pretrained=True)
-num_ftrs = model_ft.fc.in_features
-model_ft.fc = nn.Linear(num_ftrs, 2)
-
-model_ft = model_ft.to(device)
 
 criterion = nn.CrossEntropyLoss()
 
